6ffa589292d38c445978ddfcdd306b4a86659b70
WhoIsYgim <e.larkin3004@gmail.com>
2022-03-18 14:42:28 +0300

    vlad commit

---
+++ build/src/warship/CMakeFiles/warship.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warship.dir/warship.c.o"
+  "libwarship.a"
+  "libwarship.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/warship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
